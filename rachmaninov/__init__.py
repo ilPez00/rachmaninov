2fc307849d9e87b77641907a57d20cfacf2d8339
ilPez00 <pezzingiovanniantonio@gmail.com>
2026-05-19 22:03:24 +0200

    feat: export PersonalOntology and UberWiki in package root

--- rachmaninov/__init__.py
+++ rachmaninov/__init__.py
@@ -1,5 +1,7 @@
 from .models import ProposedAction, PersonalEntity, WikiPage, ScoreAxis
 from .ontology import resolve_domain, PRAXIS_ONTOLOGY
+from .ontology_personal import PersonalOntology
+from .wiki import UberWiki
 from .planner import RachmaninovPlanner
 from .actor import RachmaninovActor
 
